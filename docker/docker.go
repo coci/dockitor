ee31db7f4c4f0096a66cd6bbedd1ca0a54fa22ef
soroush safari <soroush_safarii@yahoo.com>
2022-02-21 17:04:49 +0330

    docker driver

--- docker/docker.go
+++ docker/docker.go
@@ -1,33 +1,44 @@
-package dumitor
+package docker
 
 import (
 	"context"
-	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"strings"
 )
 
+type Container struct {
+	Name   string
+	Id     string
+	Image  string
+	Status string
+}
 
+// returns docker client
 func dockerClient() *client.Client {
-    cli, err := client.NewClientWithOpts(client.FromEnv)
+	cli, err := client.NewClientWithOpts(client.FromEnv)
 
 	if err != nil {
 		panic(err)
-    }
-    return cli
+	}
+	return cli
 }
 
-func containerList() []string {
-    cli := dockerClient()
-    containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-    if err != nil {
-        panic(err)
-    }
-    var containerNames []string
-    for _, container := range containers {
-        fmt.Println(containerNames, container.Names)
-    }
-    return containerNames
+// ContainerList : Get all containers
+// it returns a list of Container structs
+func ContainerList() []Container {
+	var containerNames []Container
+
+	cli := dockerClient()
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		ctn := Container{Name: strings.Replace(container.Names[0], "/", "", -1), Id: container.ID[:10], Image: container.Image, Status: container.Status}
+		containerNames = append(containerNames, ctn)
+	}
+
+	return containerNames
 }
-
-
